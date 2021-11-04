907536b6cc140db68b2106bb82e7066bf60ad989
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-11-04 09:55:02 -0400

    Add tx specific statedb wrapper for overriding tx specific fields

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -64,6 +64,59 @@ func (n *proofList) Delete(key []byte) error {
 	panic("not supported")
 }
 
+// StateDB is an EVM database for full state querying.
+type StateDBInterface interface {
+	CreateAccount(common.Address)
+
+	SubBalance(common.Address, *big.Int)
+	AddBalance(common.Address, *big.Int)
+	GetBalance(common.Address) *big.Int
+
+	GetNonce(common.Address) uint64
+	SetNonce(common.Address, uint64)
+
+	GetCodeHash(common.Address) common.Hash
+	GetCode(common.Address) []byte
+	SetCode(common.Address, []byte)
+	GetCodeSize(common.Address) int
+
+	AddRefund(uint64)
+	SubRefund(uint64)
+	GetRefund() uint64
+
+	GetCommittedState(common.Address, common.Hash) common.Hash
+	GetState(common.Address, common.Hash) common.Hash
+	SetState(common.Address, common.Hash, common.Hash)
+
+	Suicide(common.Address) bool
+	HasSuicided(common.Address) bool
+
+	// Exist reports whether the given account exists in state.
+	// Notably this should also return true for suicided accounts.
+	Exist(common.Address) bool
+	// Empty returns whether the given account is empty. Empty
+	// is defined according to EIP161 (balance = nonce = code = 0).
+	Empty(common.Address) bool
+
+	PrepareAccessList(sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
+	AddressInAccessList(addr common.Address) bool
+	SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool)
+	// AddAddressToAccessList adds the given address to the access list. This operation is safe to perform
+	// even if the feature/fork is not active yet
+	AddAddressToAccessList(addr common.Address)
+	// AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform
+	// even if the feature/fork is not active yet
+	AddSlotToAccessList(addr common.Address, slot common.Hash)
+
+	RevertToSnapshot(int)
+	Snapshot() int
+
+	AddLog(*types.Log)
+	AddPreimage(common.Hash, []byte)
+
+	ForEachStorage(common.Address, func(common.Hash, common.Hash) bool) error
+}
+
 // StateDB structs within the ethereum protocol are used to store anything
 // within the merkle trie. StateDBs take care of caching and storing
 // nested states. It's the general query interface to retrieve:
--- core/state/statedb_tx_specific.go
+++ core/state/statedb_tx_specific.go
@@ -1,6 +1,12 @@
 package state
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
 
 // txStateContext is intended to contain all of the tx specific
 // information that the statedb tracks, so that by swapping a single
@@ -14,12 +20,259 @@ type txStateContext struct {
 	refund     uint64
 }
 
-// Plan:
-// txStateContext tracks all of the tx specific information
-// therefore, instead of using the normal statedb within the EVM,
-// we will instead access it through a txSpecificStateDB intended for
-// managed concurrent access to the statedb.
+type txSpecificStateDB struct {
+	lock *sync.Mutex
 
-// This struct will grab the statedb lock, set the txStateContext
-// do the computation, and release the statedb lock, so that during the
-// execution of the individual function call, the statedb will operate on the correct context
+	txContext *txStateContext
+	*StateDB
+}
+
+func NewTxSpecificStateDB(stateDB *StateDB, sharedLock *sync.Mutex, txHash common.Hash, txIndex int) StateDBInterface {
+	return &txSpecificStateDB{
+		lock:    sharedLock,
+		StateDB: stateDB,
+		txContext: &txStateContext{
+			journalTracker: journalTracker{
+				journal: newJournal(),
+			},
+			accessList: newAccessList(),
+			thash:      txHash,
+			txIndex:    txIndex,
+		},
+	}
+}
+
+func (txDB *txSpecificStateDB) CreateAccount(addr common.Address) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.CreateAccount(addr)
+}
+
+func (txDB *txSpecificStateDB) SubBalance(addr common.Address, amount *big.Int) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.SubBalance(addr, amount)
+}
+func (txDB *txSpecificStateDB) AddBalance(addr common.Address, amount *big.Int) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddBalance(addr, amount)
+}
+func (txDB *txSpecificStateDB) GetBalance(addr common.Address) *big.Int {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetBalance(addr)
+}
+
+func (txDB *txSpecificStateDB) GetNonce(addr common.Address) uint64 {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetNonce(addr)
+}
+func (txDB *txSpecificStateDB) SetNonce(addr common.Address, nonce uint64) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.SetNonce(addr, nonce)
+}
+
+func (txDB *txSpecificStateDB) GetCodeHash(addr common.Address) common.Hash {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetCodeHash(addr)
+}
+func (txDB *txSpecificStateDB) GetCode(addr common.Address) []byte {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetCode(addr)
+}
+func (txDB *txSpecificStateDB) SetCode(addr common.Address, code []byte) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.SetCode(addr, code)
+}
+func (txDB *txSpecificStateDB) GetCodeSize(addr common.Address) int {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetCodeSize(addr)
+}
+
+func (txDB *txSpecificStateDB) AddRefund(amount uint64) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddRefund(amount)
+}
+func (txDB *txSpecificStateDB) SubRefund(amount uint64) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.SubRefund(amount)
+}
+func (txDB *txSpecificStateDB) GetRefund() uint64 {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetRefund()
+}
+
+func (txDB *txSpecificStateDB) GetCommittedState(addr common.Address, hash common.Hash) common.Hash {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetCommittedState(addr, hash)
+}
+func (txDB *txSpecificStateDB) GetState(addr common.Address, hash common.Hash) common.Hash {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.GetState(addr, hash)
+}
+func (txDB *txSpecificStateDB) SetState(addr common.Address, key common.Hash, value common.Hash) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.SetState(addr, key, value)
+}
+
+func (txDB *txSpecificStateDB) Suicide(addr common.Address) bool {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.Suicide(addr)
+}
+func (txDB *txSpecificStateDB) HasSuicided(addr common.Address) bool {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.HasSuicided(addr)
+}
+
+// Exist reports whether the given account exists in state.
+// Notably this should also return true for suicided accounts.
+func (txDB *txSpecificStateDB) Exist(addr common.Address) bool {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.Exist(addr)
+}
+
+// Empty returns whether the given account is empty. Empty
+// is defined according to EIP161 (balance = nonce = code = 0).
+func (txDB *txSpecificStateDB) Empty(addr common.Address) bool {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.Empty(addr)
+}
+
+func (txDB *txSpecificStateDB) PrepareAccessList(sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.PrepareAccessList(sender, dest, precompiles, txAccesses)
+}
+
+func (txDB *txSpecificStateDB) AddressInAccessList(addr common.Address) bool {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.AddressInAccessList(addr)
+}
+
+func (txDB *txSpecificStateDB) SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.SlotInAccessList(addr, slot)
+}
+
+// AddAddressToAccessList adds the given address to the access list. This operation is safe to perform
+// even if the feature/fork is not active yet
+func (txDB *txSpecificStateDB) AddAddressToAccessList(addr common.Address) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddAddressToAccessList(addr)
+}
+
+// AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform {
+// even if the feature/fork is not active yet
+func (txDB *txSpecificStateDB) AddSlotToAccessList(addr common.Address, slot common.Hash) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddSlotToAccessList(addr, slot)
+}
+
+func (txDB *txSpecificStateDB) RevertToSnapshot(snapshot int) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.RevertToSnapshot(snapshot)
+}
+func (txDB *txSpecificStateDB) Snapshot() int {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.Snapshot()
+}
+
+func (txDB *txSpecificStateDB) AddLog(log *types.Log) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddLog(log)
+}
+func (txDB *txSpecificStateDB) AddPreimage(hash common.Hash, preimage []byte) {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	txDB.StateDB.AddPreimage(hash, preimage)
+}
+
+func (txDB *txSpecificStateDB) ForEachStorage(addr common.Address, f func(common.Hash, common.Hash) bool) error {
+	txDB.lock.Lock()
+	defer txDB.lock.Unlock()
+
+	txDB.StateDB.txStateContext = txDB.txContext
+	return txDB.StateDB.ForEachStorage(addr, f)
+}
